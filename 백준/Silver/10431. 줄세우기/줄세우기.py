8cc0bf8f568b632d5f50b5baeba3e1b912325f1c
imdana <47177682+iam-dana@users.noreply.github.com>
2024-02-20 12:54:38 +0900

    [Silver V] Title: 줄세우기, Time: 88 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+P = int(input())
+
+for p in range(1, P + 1):
+    case = deque(list(map(int, input().split())))
+    case.popleft()
+    result = []
+    ans = 0
+    while case:
+        n = case.popleft()
+        if not result:
+            result.append(n)
+        else:
+            flag = 0
+            for i in range(len(result)):
+                if n < result[i]:
+                    result.insert(i, n)
+                    ans += len(result)-i-1
+                    flag = 1
+                    break
+            if not flag:
+                result.append(n)
+
+
+    print("{} {}".format(p, ans))
