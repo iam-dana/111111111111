27f01c80877e5acc43978455e57a748dfa1eaed1
imdana <47177682+iam-dana@users.noreply.github.com>
2024-03-13 14:24:24 +0900

    [Gold IV] Title: 인구 이동, Time: 928 ms, Memory: 124208 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,53 +1,54 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-n, l, r = map(int, input().split())
-graph = []
-ans = 0
-for i in range(n):
-    graph.append(list(map(int, input().split())))
+N, L, R = map(int, input().split())  # L <= (인구 차이) <= R 일 시 국경이 열린다.
+area = [list(map(int, input().split())) for _ in range(N)]
+
+
+def isEdge(x, y):
+    if not (0 <= x < N) or not (0 <= y < N):
+        return True
+    return False
+
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-
-
-def move(u):
-    h = 0
-    for x, y in u:
-        h += graph[x][y]
-    h = h // len(u)
-    for x, y in u:
-        graph[x][y] = h
-    return
-
+answer = 0
 
 while 1:
-    e_key = 0
-    visited = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            unite = []
-            q = deque()
+    visited = [[0] * N for _ in range(N)]
+    union = []
+    unionPopulation = []
+    for i in range(N):
+        for j in range(N):
             if not visited[i][j]:
-                q.append([i, j])
                 visited[i][j] = 1
-                unite.append([i, j])
+                q = deque()
+                q.append([i, j])
+                tmp = [[i, j]]
+                population = area[i][j]
                 while q:
                     x, y = q.popleft()
                     for k in range(4):
-                        nx, ny = x+dx[k], y+dy[k]
-                        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and l <= abs(graph[x][y] - graph[nx][ny]) <= r:
-                            visited[nx][ny] = 1
-                            q.append([nx, ny])
-                            unite.append([nx, ny])
-                if len(unite) > 1:
-                    move(unite)
-                    e_key += 1
-    if not e_key:
+                        nx = x + dx[k]
+                        ny = y + dy[k]
+                        if not isEdge(nx, ny) and not visited[nx][ny]:
+                            if L <= abs(area[nx][ny] - area[x][y]) <= R:
+                                population += area[nx][ny]
+                                q.append([nx, ny])
+                                visited[nx][ny] = 1
+                                tmp.append([nx, ny])
+                if len(tmp) > 1:
+                    union.append(tmp)
+                    unionPopulation.append(population // len(tmp))
+    if not union:
         break
-    else:
-        ans += 1
+    for i in range(len(union)):
+        u = union[i]
+        for ux, uy in u:
+            area[ux][uy] = unionPopulation[i]
+    answer += 1
 
-print(ans)
-
+print(answer)
