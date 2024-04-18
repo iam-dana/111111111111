19cdb2c0d7a9596e845451d9369652459213ea01
imdana <47177682+iam-dana@users.noreply.github.com>
2024-04-18 16:34:55 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 124 ms, Memory: 110432 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,21 +1,19 @@
 import sys
+
 input = sys.stdin.readline
 
 S = list(input().rstrip())
-now = []
+idx = 0
 
 for i in range(1, sys.maxsize):
-    now.extend(str(i))
+    now = list(str(i))
 
-    idx = 0
-
-    for j in range(len(now)-idx):
-        # print(now[j], end=" ")
+    for j in range(len(now)):
         if idx == len(S):
             break
         if S[idx] == now[j]:
             idx += 1
-    # print()
+
     if idx == len(S):
         print(i)
         exit()
