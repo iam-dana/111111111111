1696ab048d6d184e7b2b7ab0e3181b2be9786b92
imdana <47177682+iam-dana@users.noreply.github.com>
2025-01-01 13:25:48 +0900

    [Gold IV] Title: 동전 1, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[k+1];
+
+        for (int i=1; i<=n; i++) {
+            int coin = Integer.parseInt(br.readLine());
+            if (coin <= k) {
+                dp[coin]++;
+                for (int j=coin+1; j<=k; j++) {
+                    dp[j] += dp[j-coin];
+                }
+            }
+        }
+        System.out.println(dp[k]);
+    }
+}
